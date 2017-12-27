7bccaa7bfc143abcaca7dea134df2b5dac8557cb
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-27 19:51:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten(){
  return kittens.pop()
 }
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
